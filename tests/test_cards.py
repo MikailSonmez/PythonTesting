4d236e70b0596439a46375ba4dab7616f5936b0b
Mikail Sonmez <mikail4458@hotmail.com>
2022-08-15 14:06:10 +0300

    v2

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -14,3 +14,16 @@ def test_ice_spirit_is_displayed():
     # 3. assert Ice Spirit is displayed
     ice_spirit_card = driver.find_element(By.CSS_SELECTOR, "[href*='Ice+Spirit']")
     assert ice_spirit_card.is_displayed()
+
+    # 4. get the card, name, type. arena, and rarity
+    card_name = driver.find_element(By.CSS_SELECTOR, "[class*='cardName']").text
+    split = driver.find_element(By.CSS_SELECTOR, "[class*='card__rarity']").text.split(', ')
+    card_type = split[0]
+    card_arena = split[1]
+    card_rarity = driver.find_element(By.CSS_SELECTOR, "[class*=rarityCaption']").text.split('\n')[1]
+
+    # 5. assert they are correct
+    assert card_name == 'Ice Sprit'
+    assert card_type == 'Troop'
+    assert card_arena == 'Arena 8'
+    assert card_rarity == 'Common'
