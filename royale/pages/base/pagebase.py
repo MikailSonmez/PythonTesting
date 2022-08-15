f17a7834b2d7d41be2b232897c44e12eeaf1a113
Mikail Sonmez <mikail4458@hotmail.com>
2022-08-15 15:43:28 +0300

    v3

---
+++ royale/pages/base/pagebase.py
@@ -0,0 +1,11 @@
+from royale.pages.headernav import HeaderNav
+
+
+class PageBase:
+    def __init__(self, driver):
+        self.headernav = HeaderNav(driver)
+
+
+
+
+
--- tests/test_cards.py
+++ tests/test_cards.py
@@ -1,26 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from royale.pages.base.card_details_page import CardDetailsPage
+from royale.pages.cards_page import CardsPage
+
 
 def test_ice_spirit_is_displayed():
+    driver = webdriver.Chrome()
+    driver.get('https://statsroyale.com')
+
+    cards_page = CardsPage(driver).goto()
+    ice_spirit = cards_page.get_card_by_name('Ice Spirit')
+    assert ice_spirit.is_displayed()
+
+
+def test_ice_spirit_details_displayed():
     driver = webdriver.Chrome()
 
     # 1. go to statsroyale.com
     driver.get('https://statsroyale.com')
 
     # 2. go to cards page
-    driver.find_element(By.CSS_SELECTOR, "[href='/cards']").click()
-
     # 3. assert Ice Spirit is displayed
-    ice_spirit_card = driver.find_element(By.CSS_SELECTOR, "[href*='Ice+Spirit']")
-    assert ice_spirit_card.is_displayed()
+    CardsPage(driver).goto().get_card_by_name('Ice Spirit').click()
 
     # 4. get the card, name, type. arena, and rarity
-    card_name = driver.find_element(By.CSS_SELECTOR, "[class*='cardName']").text
-    split = driver.find_element(By.CSS_SELECTOR, "[class*='card__rarity']").text.split(', ')
+    details_page = CardDetailsPage(driver)
+    card_name = details_page.map.card_name.text
+    split = details_page.map.card_category.text.split(', ')
     card_type = split[0]
     card_arena = split[1]
-    card_rarity = driver.find_element(By.CSS_SELECTOR, "[class*=rarityCaption']").text.split('\n')[1]
+    card_rarity = details_page.map.card_rarity.text.split('\n')[1]
 
     # 5. assert they are correct
     assert card_name == 'Ice Sprit'
